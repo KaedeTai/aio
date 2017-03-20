c02a8adf9fc1e11fe92b72fd76b121eb78d0795f
kaede <kaede@KaedeMac.local>
2017-03-20 22:03:53 +0800

    新增通用資料庫處理函示

--- utils/DB.js
+++ utils/DB.js
@@ -13,4 +13,51 @@ var DB = (sql, params) => await(db.query(sql, params).spread(rs => rs));
 DB.one = (sql, params) => DB(sql, params)[0];
 DB.quote = (table) => '`' + table.replace(/`/g, '') + '`';
 
+DB.tables = () => DB('SHOW TABLES');
+
+DB.explain = (table) => DB('EXPLAIN ' + DB.quote(table));
+
+DB.select = (table, fields) => {
+  if (!fields) fields = '*';
+  return DB('SELECT ' + fields + ' FROM ' + DB.quote(table));
+}
+
+DB.insert = (table, values) => {
+  var keys = [];
+  var vars = [];
+  var vals = [];
+  for (k in values) {
+    keys.push(DB.quote(k));
+    vars.push('?');
+    vals.push(values[k]);
+  }
+  var sql = 'INSERT INTO ' + DB.quote(table) + ' (' + keys.join(',') + ') VALUES (' + vars.join(',') + ')';
+  return DB(sql, vals);
+};
+
+DB.replace = (table, values) => {
+  var keys = [];
+  var vars = [];
+  var vals = [];
+  for (k in values) {
+    keys.push(DB.quote(k));
+    vars.push('?');
+    vals.push(values[k]);
+  }
+  var sql = 'REPLACE INTO ' + DB.quote(table) + ' (' + keys.join(',') + ') VALUES (' + vars.join(',') + ')';
+  return DB(sql, vals);
+};
+
+DB.delete = (table, values) => {
+  var keys = [];
+  var vars = [];
+  var vals = [];
+  for (k in values) {
+    vars.push(DB.quote(k) + '=?');
+    vals.push(values[k]);
+  }
+  var sql = 'DELETE FROM ' + DB.quote(table) + ' WHERE ' + vars.join(' AND ');
+  return DB(sql, vals);
+};
+
 module.exports = DB;
