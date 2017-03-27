120f257207dfbb85b6d79bed0afa66b0f7cdd893
kaede <kaede@KaedeMac.lan>
2017-03-27 17:51:29 +0800

    刪除多餘行

--- models/Car.js
+++ models/Car.js
@@ -1,8 +1,17 @@
 var DB = require('../utils/DB');
-var util = require('util');
 
 //可以不換行，DB.one('xxx') 跟 DB('xxx')[0] 是一樣的意思，只取第一筆
-Car = (id) => DB.one(`SELECT * FROM car WHERE id=?`, [id]);
+Car = (id) => DB.select('car', {id: id});
+
+Car.getAllInfo = (id, gender) => DB(`
+SELECT a.aaa, b.bbb, c.ccc
+  FROM aa a, bb b, cc ca
+ WHERE a.bb_id = b.id
+   AND b.cc_id = c.id
+   AND b.id = :id
+   AND c.id = :id
+   AND c.gender = :gender
+`, {id, gender});
 
 //也可以換行，下 INSERT/UPDATE/DELETE 只能用 DB('xxx') 不能用 DB.one('xxx')
 Car.delete = (id) => DB(`
